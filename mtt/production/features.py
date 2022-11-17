0f3b1e2cec0819f93a4c702a706f10c550d525ed
Daniel Savoiu <daniel.savoiu@cern.ch>
2022-11-17 15:49:45 +0100

    Add cutflow features for both AK4 and AK8 jets.

--- mtt/production/features.py
+++ mtt/production/features.py
@@ -35,10 +35,14 @@ def jet_energy_shifts_init(self: Producer) -> None:
 
 @producer(
     uses={
-        "Electron.pt", "Muon.pt", "Jet.pt", "BJet.pt",
+        "Muon.pt", "Jet.pt", "BJet.pt",
     },
     produces={
-        "ht", "n_jet", "n_electron", "n_muon",
+        "ht_ak4",
+        "n_jet_ak4",
+        "ht_ak8",
+        "n_jet_ak8",
+        "n_muon",
     },
     shifts={
         jet_energy_shifts,
@@ -58,10 +62,15 @@ def features(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
 
 @producer(
     uses={
-        mc_weight, category_ids, "Jet.pt",
+        mc_weight, category_ids,
+        "Jet.pt", "FatJet.pt",
     },
     produces={
-        mc_weight, category_ids, "cutflow.n_jet", "cutflow.ht", "cutflow.jet1_pt",
+        mc_weight, category_ids,
+        "cutflow.n_jet_ak4", "cutflow.ht_ak4",
+        "cutflow.jet_ak4_1_pt", "cutflow.jet_ak4_2_pt", "cutflow.jet_ak4_3_pt", "cutflow.jet_ak4_4_pt",
+        "cutflow.n_jet_ak8", "cutflow.ht_ak8",
+        "cutflow.jet_ak8_1_pt", "cutflow.jet_ak8_2_pt", "cutflow.jet_ak8_3_pt", "cutflow.jet_ak8_4_pt",
     },
 )
 def cutflow_features(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
@@ -69,10 +78,20 @@ def cutflow_features(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     events = self[mc_weight](events, **kwargs)
     events = self[category_ids](events, **kwargs)
 
-    events = set_ak_column(events, "cutflow.n_jet", ak.num(events.Jet, axis=1))
+    events = set_ak_column(events, "cutflow.n_jet_ak4", ak.num(events.Jet, axis=1))
+    events = set_ak_column(events, "cutflow.n_jet_ak8", ak.num(events.FatJet, axis=1))
 
-    events = set_ak_column(events, "cutflow.ht", ak.sum(events.Jet.pt, axis=1))
+    events = set_ak_column(events, "cutflow.ht_ak4", ak.sum(events.Jet.pt, axis=1))
+    events = set_ak_column(events, "cutflow.ht_ak8", ak.sum(events.FatJet.pt, axis=1))
 
-    events = set_ak_column(events, "cutflow.jet1_pt", Route("Jet.pt[:,0]").apply(events, EMPTY_FLOAT))
+    events = set_ak_column(events, "cutflow.jet_ak4_1_pt", Route("Jet.pt[:,0]").apply(events, EMPTY_FLOAT))
+    events = set_ak_column(events, "cutflow.jet_ak4_2_pt", Route("Jet.pt[:,1]").apply(events, EMPTY_FLOAT))
+    events = set_ak_column(events, "cutflow.jet_ak4_3_pt", Route("Jet.pt[:,2]").apply(events, EMPTY_FLOAT))
+    events = set_ak_column(events, "cutflow.jet_ak4_4_pt", Route("Jet.pt[:,3]").apply(events, EMPTY_FLOAT))
+
+    events = set_ak_column(events, "cutflow.jet_ak8_1_pt", Route("FatJet.pt[:,0]").apply(events, EMPTY_FLOAT))
+    events = set_ak_column(events, "cutflow.jet_ak8_2_pt", Route("FatJet.pt[:,1]").apply(events, EMPTY_FLOAT))
+    events = set_ak_column(events, "cutflow.jet_ak8_3_pt", Route("FatJet.pt[:,2]").apply(events, EMPTY_FLOAT))
+    events = set_ak_column(events, "cutflow.jet_ak8_4_pt", Route("FatJet.pt[:,3]").apply(events, EMPTY_FLOAT))
 
     return events
