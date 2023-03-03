5ce2fdeac492b9296ff913aa6e4cdaa766375860
Johanna Matthiesen <jomatthi@physnet.uni-hamburg.de>
2023-03-03 13:36:42 +0100

    Add jet lepton cleaning.

--- mtt/calibration/jets.py
+++ mtt/calibration/jets.py
@@ -7,6 +7,8 @@ Custom jet energy calibration methods that disable data uncertainties (for searc
 from columnflow.calibration import Calibrator, calibrator
 from columnflow.calibration.cms.jets import jec, jer
 from columnflow.util import maybe_import
+from columnflow.production.util import attach_coffea_behavior
+from columnflow.columnar_util import set_ak_column
 
 ak = maybe_import("awkward")
 
@@ -42,3 +44,73 @@ def jet_energy_init(self: Calibrator) -> None:
     else:
         self.uses |= {jec_nominal}
         self.produces |= {jec_nominal}
+
+
+@calibrator(
+    uses={
+        "Electron.pt", "Electron.eta", "Electron.phi", "Electron.mass", "nElectron",
+        "Muon.pt", "Muon.eta", "Muon.phi", "Muon.mass", "nMuon",
+        "Jet.pt", "Jet.eta", "Jet.phi", "Jet.mass", "Jet.rawFactor", "nJet",
+        "Jet.muonIdx1", "Jet.muonIdx2", "Jet.electronIdx1", "Jet.electronIdx2",
+        attach_coffea_behavior,
+    }
+    produces={
+        "Jet.pt", "Jet.eta", "Jet.phi", "Jet.mass", "Jet.rawFactor",
+    }
+)
+def jet_lepton_cleaner(self: Calibrator, events: ak.Array, **kwargs) -> ak.Array:
+    """
+    Calibrator to clean jet four-vectors from contributions from nearby leptons
+    """
+
+    # load coffea behaviors for simplified arithmetic with vectors
+    events["Electron"] = ak.with_name(events.Electron, "PtEtaPhiMLorentzVector")
+    events["Muon"] = ak.with_name(events.Muon, "PtEtaPhiMLorentzVector")
+    events["Jet"] = ak.with_name(events.Jet, "PtEtaPhiMLorentzVector")
+
+    # revert JEC for jet pt and jet mass,
+    # set correction factor to 0
+    events = set_ak_column(events, "Jet.pt", events.Jet.pt * (1 - events.Jet.rawFactor))
+    events = set_ak_column(events, "Jet.mass", events.Jet.mass * (1 - events.Jet.rawFactor))
+    events = set_ak_column(events, "Jet.rawFactor", 0)
+
+    # create arrays with indices of leptons that are matched to jet,
+    # non if no matched lepton
+    idx_e1 = ak.mask(events.Jet.electronIdx1, events.Jet.electronIdx1 >= 0)
+    idx_e2 = ak.mask(events.Jet.electronIdx2, events.Jet.electronIdx2 >= 0)
+    idx_m1 = ak.mask(events.Jet.muonIdx1, events.Jet.muonIdx1 >= 0)
+    idx_m2 = ak.mask(events.Jet.muonIdx2, events.Jet.muonIdx2 >= 0)
+
+    # list with matched leptons
+    jet_leptons = [
+        events.Electron[idx_e1],
+        events.Electron[idx_e2],
+        events.Muon[idx_m1],
+        events.Muon[idx_m2],
+    ]
+
+    # convert to four-vectors for leptons in jets
+    jet_lepton_p4s = [
+        ak.with_name(
+            ak.zip(
+                {var: getattr(jet_lepton, var) for var in ["pt", "eta", "phi", "mass"]}
+            ),
+            "PtEtaPhiMLorentzVector"
+        )
+        for jet_lepton in jet_leptons
+    ]
+
+    # sum lepton contributions
+    jet_lepton_sum = ak.concatenate(
+        [ak.singletons(jet_lepton_p4, axis=1) for jet_lepton_p4 in jet_lepton_p4s],
+        axis=2,
+    ).sum(axis=2)
+
+    # substract lepton contributions from jets
+    jet_cleaned = ak.with_name(events.Jet - jet_lepton_sum, "LorentzVector")
+
+    # save updated jet variables
+    for var in ["pt", "eta", "phi", "mass"]:
+        events = set_ak_column(events, f"Jet.{var}", getattr(jet_cleaned, var))
+
+    return events
