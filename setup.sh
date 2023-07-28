4222bd59bc1152c1324660431a37fd2cced71896
Daniel Savoiu <daniel.savoiu@cern.ch>
2023-07-28 14:35:08 +0200

    Adapt setup.sh to upstream changes.

--- setup.sh
+++ setup.sh
@@ -37,6 +37,11 @@ setup_mtt() {
     local setup_is_default="false"
     [ "${setup_name}" = "default" ] && setup_is_default="true"
 
+    # zsh options
+    if ${shell_is_zsh}; then
+        setopt globdots
+    fi
+
 
     #
     # global variables
@@ -46,8 +51,6 @@ setup_mtt() {
     # start exporting variables
     export MTT_BASE="${this_dir}"
     export CF_BASE="${this_dir}/modules/columnflow"
-    export CF_REPO_BASE="${MTT_BASE}"
-    export CF_REPO_BASE_ALIAS="MTT_BASE"
     export CF_SETUP_NAME="${setup_name}"
 
     # load cf setup helpers
@@ -55,7 +58,7 @@ setup_mtt() {
 
     # interactive setup
     if [ "${CF_REMOTE_JOB}" != "1" ]; then
-    	cf_setup_interactive_body() {
+        cf_setup_interactive_body() {
             # start querying for variables
             query CF_CERN_USER "CERN username" "$( whoami )"
             export_and_save CF_CERN_USER_FIRSTCHAR "\${CF_CERN_USER:0:1}"
@@ -67,26 +70,48 @@ setup_mtt() {
             export_and_save CF_WLCG_CACHE_CLEANUP "${CF_WLCG_CACHE_CLEANUP:-false}"
             query CF_SOFTWARE_BASE "Local directory for installing software" "\$CF_DATA/software"
             query CF_JOB_BASE "Local directory for storing job files" "\$CF_DATA/jobs"
+            query CF_VENV_SETUP_MODE_UPDATE "Automatically update virtual envs if needed" "False"
+            [ "${CF_VENV_SETUP_MODE_UPDATE}" != "True" ] && export_and_save CF_VENV_SETUP_MODE "update"
+            unset CF_VENV_SETUP_MODE_UPDATE
             query CF_VOMS "Virtual-organization" "cms:/cms/dcms"
             export_and_save CF_TASK_NAMESPACE "${CF_TASK_NAMESPACE:-cf}"
             query CF_LOCAL_SCHEDULER "Use a local scheduler for law tasks" "True"
             if [ "${CF_LOCAL_SCHEDULER}" != "True" ]; then
-		query CF_SCHEDULER_HOST "Address of a central scheduler for law tasks" "naf-cms15.desy.de"
-		query CF_SCHEDULER_PORT "Port of a central scheduler for law tasks" "8082"
+                query CF_SCHEDULER_HOST "Address of a central scheduler for law tasks" "127.0.0.1"
+                query CF_SCHEDULER_PORT "Port of a central scheduler for law tasks" "8082"
             else
-		export_and_save CF_SCHEDULER_HOST "127.0.0.1"
-		export_and_save CF_SCHEDULER_PORT "8082"
+                export_and_save CF_SCHEDULER_HOST "127.0.0.1"
+                export_and_save CF_SCHEDULER_PORT "8082"
             fi
             query MTT_BUNDLE_CMSSW "Install and bundle CMSSW sandboxes for job submission?" "True"
-	}
-	cf_setup_interactive "${CF_SETUP_NAME}" "${MTT_BASE}/.setups/${CF_SETUP_NAME}.sh" || return "$?"
+        }
+        cf_setup_interactive "${CF_SETUP_NAME}" "${MTT_BASE}/.setups/${CF_SETUP_NAME}.sh" || return "$?"
     fi
 
     # continue the fixed setup
+    export CF_REPO_BASE="${MTT_BASE}"
+    export CF_REPO_BASE_ALIAS="MTT_BASE"
     export CF_CONDA_BASE="${CF_CONDA_BASE:-${CF_SOFTWARE_BASE}/conda}"
     export CF_VENV_BASE="${CF_VENV_BASE:-${CF_SOFTWARE_BASE}/venvs}"
     export CF_CMSSW_BASE="${CF_CMSSW_BASE:-${CF_SOFTWARE_BASE}/cmssw}"
     export CF_CI_JOB="$( [ "${GITHUB_ACTIONS}" = "true" ] && echo 1 || echo 0 )"
+    export CF_ORIG_PATH="${PATH}"
+    export CF_ORIG_PYTHONPATH="${PYTHONPATH}"
+    export CF_ORIG_PYTHON3PATH="${PYTHON3PATH}"
+    export CF_ORIG_LD_LIBRARY_PATH="${LD_LIBRARY_PATH}"
+
+    # show a warning in case no CF_REPO_BASE_ALIAS is set
+    if [ -z "${CF_REPO_BASE_ALIAS}" ]; then
+        cf_color yellow "the variable CF_REPO_BASE_ALIAS is unset"
+        cf_color yellow "please consider setting it to the name of the variable that refers to your analysis base directory"
+    fi
+
+
+    #
+    # minimal local software setup
+    #
+
+    cf_setup_software_stack "${CF_SETUP_NAME}" || return "$?"
 
 
     #
@@ -97,10 +122,10 @@ setup_mtt() {
 
 
     #
-    # minimal local software setup
+    # git hooks
     #
 
-    cf_setup_software_stack "${CF_SETUP_NAME}" || return "$?"
+    cf_setup_git_hooks || return "$?"
 
     # ammend paths that are not covered by the central cf setup
     export PATH="${MTT_BASE}/bin:${PATH}"
