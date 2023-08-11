665d0f93b00d6982cf4daafb40940003f0d9ebdc
Daniel Savoiu <daniel.savoiu@cern.ch>
2023-08-11 09:19:11 +0200

    Collect external files sources in a dict for convenient use.

--- mtt/config/config_2017.py
+++ mtt/config/config_2017.py
@@ -809,34 +809,38 @@ def make_jme_filename(jme_aux, sample_type, name, era=None):
 
 
 # external files
-json_mirror = "/afs/cern.ch/user/m/mrieger/public/mirrors/jsonpog-integration-dfd90038"
+sources = {
+    "cert": "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV",
+    "local_repo": os.getenv("MTT_ORIG_BASE"),
+    "json_mirror": "/afs/cern.ch/user/m/mrieger/public/mirrors/jsonpog-integration-dfd90038",
+}
 config_2017.x.external_files = DotDict.wrap({
     # jet energy corrections
-    "jet_jerc": (f"{json_mirror}/POG/JME/2017_UL/jet_jerc.json.gz", "v1"),  # noqa
+    "jet_jerc": (f"{sources['json_mirror']}/POG/JME/2017_UL/jet_jerc.json.gz", "v1"),  # noqa
 
     # btag scale factors
-    "btag_sf_corr": (f"{json_mirror}/POG/BTV/2017_UL/btagging.json.gz", "v1"),  # noqa
+    "btag_sf_corr": (f"{sources['json_mirror']}/POG/BTV/2017_UL/btagging.json.gz", "v1"),  # noqa
 
     # electron scale factors
-    "electron_sf": (f"{json_mirror}/POG/EGM/2017_UL/electron.json.gz", "v1"),  # noqa
+    "electron_sf": (f"{sources['json_mirror']}/POG/EGM/2017_UL/electron.json.gz", "v1"),  # noqa
 
     # muon scale factors
-    "muon_sf": (f"{json_mirror}/POG/MUO/2017_UL/muon_Z.json.gz", "v1"),  # noqa
+    "muon_sf": (f"{sources['json_mirror']}/POG/MUO/2017_UL/muon_Z.json.gz", "v1"),  # noqa
 
-    # files from TODO
+    # lumi files
     "lumi": {
-        "golden": ("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt", "v1"),  # noqa
+        "golden": (f"{sources['cert']}/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt", "v1"),  # noqa
         "normtag": ("/afs/cern.ch/user/l/lumipro/public/Normtags/normtag_PHYSICS.json", "v1"),
     },
 
     # files from https://twiki.cern.ch/twiki/bin/viewauth/CMS/PileupJSONFileforData?rev=44#Pileup_JSON_Files_For_Run_II
     "pu": {
-        "json": ("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PileUp/UltraLegacy/pileup_latest.txt", "v1"),  # noqa
+        "json": (f"{sources['cert']}/PileUp/UltraLegacy/pileup_latest.txt", "v1"),  # noqa
         "mc_profile": ("https://raw.githubusercontent.com/cms-sw/cmssw/435f0b04c0e318c1036a6b95eb169181bbbe8344/SimGeneral/MixingModule/python/mix_2017_25ns_UltraLegacy_PoissonOOTPU_cfi.py", "v1"),  # noqa
         "data_profile": {
-            "nominal": ("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PileUp/UltraLegacy/PileupHistogram-goldenJSON-13tev-2017-69200ub-99bins.root", "v1"),  # noqa
-            "minbias_xs_up": ("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PileUp/UltraLegacy/PileupHistogram-goldenJSON-13tev-2017-72400ub-99bins.root", "v1"),  # noqa
-            "minbias_xs_down": ("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PileUp/UltraLegacy/PileupHistogram-goldenJSON-13tev-2017-66000ub-99bins.root", "v1"),  # noqa
+            "nominal": (f"{sources['cert']}/PileUp/UltraLegacy/PileupHistogram-goldenJSON-13tev-2017-69200ub-99bins.root", "v1"),  # noqa
+            "minbias_xs_up": (f"{sources['cert']}/PileUp/UltraLegacy/PileupHistogram-goldenJSON-13tev-2017-72400ub-99bins.root", "v1"),  # noqa
+            "minbias_xs_down": (f"{sources['cert']}/PileUp/UltraLegacy/PileupHistogram-goldenJSON-13tev-2017-66000ub-99bins.root", "v1"),  # noqa
         },
     },
 })
