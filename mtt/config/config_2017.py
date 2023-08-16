742937ffd95c1a281220e57ba86c555f3691f89a
Daniel Savoiu <daniel.savoiu@cern.ch>
2023-08-16 11:31:41 +0200

    Disable top pT reweighting for now.

--- mtt/config/config_2017.py
+++ mtt/config/config_2017.py
@@ -1036,8 +1036,8 @@ config_2017.x.event_weights = DotDict({
 for dataset in config_2017.datasets:
     dataset.x.event_weights = DotDict()
     if dataset.has_tag("is_ttbar"):
-        # top pt reweighting
-        dataset.x.event_weights["top_pt_weight"] = get_shifts("top_pt")
+        # top pt reweighting (disable for now)
+        # dataset.x.event_weights["top_pt_weight"] = get_shifts("top_pt")
     if dataset.has_tag("is_v_jets"):
         # V+jets QCD NLO reweighting
         dataset.x.event_weights["vjets_weight"] = get_shifts("vjets")
