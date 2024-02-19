c57e0af565725690338ff745bfac87b22491f7a4
Daniel Savoiu <daniel.savoiu@cern.ch>
2024-02-19 17:13:00 +0100

    CF update: use categorizers instead of selectors for categories.

--- mtt/categorization/util.py
+++ mtt/categorization/util.py
@@ -7,7 +7,7 @@ from typing import Optional
 
 from columnflow.columnar_util import Route, TaskArrayFunction
 from columnflow.util import maybe_import
-from columnflow.selection import Selector, selector
+from columnflow.selection import Categorizer, categorizer
 
 np = maybe_import("numpy")
 ak = maybe_import("awkward")
@@ -27,14 +27,14 @@ def masked_sorted_indices(
     return indices[mask[indices]]
 
 
-# -- helper functions for constructing selectors
+# -- helper functions for constructing categorizers
 
-def make_selector_not(name: str, input_selector: Selector):
+def make_categorizer_not(name: str, input_categorizer: Categorizer):
     """
-    Construct a selector that corresponds to the logical *NOT* of a given input selector.
+    Construct a categorizer that corresponds to the logical *NOT* of a given input categorizer.
     """
-    @selector(cls_name=name, uses={input_selector})
-    def sel(self: Selector, events: ak.Array, **kwargs) -> ak.Array:
+    @categorizer(cls_name=name, uses={input_categorizer})
+    def cat(self: Categorizer, events: ak.Array, **kwargs) -> ak.Array:
 
         # ensure dependencies are present
         for dep in self.uses:
@@ -42,19 +42,20 @@ def make_selector_not(name: str, input_selector: Selector):
                 continue
             events = self[dep](events, **kwargs)
 
-        # return logical NOT of input selector mask
-        return ~self[input_selector](events, **kwargs)
+        # return logical NOT of input categorizer mask
+        events, input_mask = self[input_categorizer](events, **kwargs)
+        return events, ~input_mask
 
-    return sel
+    return cat
 
 
-def make_selector_and(name: str, selectors: set[Selector]):
+def make_categorizer_and(name: str, categorizers: set[Categorizer]):
     """
-    Construct a selector that corresponds to the logical *AND* of a set of dependent selectors.
+    Construct a categorizer that corresponds to the logical *AND* of a set of dependent categorizers.
     """
 
-    @selector(cls_name=name, uses=set(selectors))
-    def sel(self: Selector, events: ak.Array, **kwargs) -> ak.Array:
+    @categorizer(cls_name=name, uses=set(categorizers))
+    def cat(self: Categorizer, events: ak.Array, **kwargs) -> ak.Array:
 
         # ensure dependencies are present
         for dep in self.uses:
@@ -62,19 +63,25 @@ def make_selector_and(name: str, selectors: set[Selector]):
                 continue
             events = self[dep](events, **kwargs)
 
-        # return logical AND of all input selector masks
-        return ak.all(
+        input_masks = [
+            self[input_categorizer](events, **kwargs)[1]
+            for input_categorizer in uses
+        ]
+
+        # return logical AND of all input categorizer masks
+        mask = ak.all(
             ak.concatenate(
-                [ak.singletons(self[s](events, **kwargs)) for s in self.uses],
+                [ak.singletons(mask) for mask in input_masks],
                 axis=1,
             ),
             axis=1,
         )
+        return events, mask
 
-    return sel
+    return cat
 
 
-def make_selector_range(
+def make_categorizer_range(
     name: str,
     route: str,
     min_val: float,
@@ -83,7 +90,7 @@ def make_selector_range(
     **decorator_kwargs,
 ):
     """
-    Construct a selector that evaluates to *True* whenever the value of the specified *route*
+    Construct a categorizer that evaluates to *True* whenever the value of the specified *route*
     lies between *min_val* and *max_val*. If supplied, an *route_func* is applied to the route
     value before performing the comparison.
     """
@@ -91,8 +98,8 @@ def make_selector_range(
     route_func_name = getattr(route_func, __name__, "<lambda>")
     route_repr = f"{route_func_name}({route})" if route_func else route
 
-    @selector(cls_name=name, **decorator_kwargs)
-    def sel(self: Selector, events: ak.Array, **kwargs) -> ak.Array:
+    @categorizer(cls_name=name, **decorator_kwargs)
+    def cat(self: Categorizer, events: ak.Array, **kwargs) -> ak.Array:
         f"""Select only events where value of {route_repr} is in range ({min_val}, {max_val})."""
 
         # ensure dependencies are present
@@ -107,6 +114,7 @@ def make_selector_range(
             val = route_func(val)
 
         # return selection mask
-        return (min_val <= val) & (val < max_val)
+        mask = (min_val <= val) & (val < max_val)
+        return events, mask
 
-    return sel
+    return cat
