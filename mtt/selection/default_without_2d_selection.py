7cef5ccb7f5c1e30a67f6ef073621569dbd31b51
Daniel Savoiu <daniel.savoiu@cern.ch>
2024-03-06 14:39:51 +0100

    Linting.

--- mtt/selection/default_without_2d_selection.py
+++ mtt/selection/default_without_2d_selection.py
@@ -144,6 +144,7 @@ def default_without_2d_selection(
 
     return events, results
 
+
 @default_without_2d_selection.init
 def default_init(self: Selector) -> None:
 
@@ -153,4 +154,5 @@ def default_init(self: Selector) -> None:
 
     if hasattr(self, "dataset_inst") and not self.dataset_inst.is_mc:
         self.uses |= {data_trigger_veto}
+
         self.produces |= {data_trigger_veto}
--- mtt/selection/qcd_spikes.py
+++ mtt/selection/qcd_spikes.py
@@ -4,29 +4,10 @@
 Selection reducing spiking QCD behavior.
 """
 
-from operator import and_
-from functools import reduce
-from collections import defaultdict
-
 from columnflow.util import maybe_import
 from columnflow.production.util import attach_coffea_behavior
 
 from columnflow.selection import Selector, SelectionResult, selector
-from columnflow.selection.cms.met_filters import met_filters
-from columnflow.selection.cms.json_filter import json_filter
-from columnflow.production.categories import category_ids
-from columnflow.production.cms.mc_weight import mc_weight
-from columnflow.production.processes import process_ids
-
-from mtt.selection.util import masked_sorted_indices
-from mtt.selection.general import increment_stats, jet_energy_shifts
-from mtt.selection.lepton import lepton_selection
-from mtt.selection.cutflow_features import cutflow_features
-from mtt.selection.early import check_early
-
-from mtt.production.lepton import choose_lepton
-from mtt.production.gen_top import gen_parton_top
-from mtt.production.gen_v import gen_v_boson
 
 np = maybe_import("numpy")
 ak = maybe_import("awkward")
