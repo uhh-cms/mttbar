4069a039d06b1cf9d239a5e2593b96aaefa0ab03
Johanna Matthiesen <jomatthi@physnet.uni-hamburg.de>
2026-09-08 16:47:38 +0200

    Rework inference model to make it more flexible and fleshed out.

--- mtt/config/variables.py
+++ mtt/config/variables.py
@@ -309,6 +309,23 @@ def add_variables(config: od.Config) -> None:
         y_title="Events",
     )
     config.add_variable(
+        # binning adjusted to AN v12
+        name="ttbar_mass_inf",
+        expression="TTbar.mass",
+        binning=[
+            0., 350., 550., 600., 650., 700., 750., 800., 850.,
+            900., 950., 1000., 1050., 1100., 1150., 1200., 1250.,
+            1300., 1350., 1400., 1450., 1500., 1550., 1600., 1650.,
+            1700., 1750., 1800., 1850., 1900., 1950., 2000., 2050.,
+            2100., 2150., 2200., 2250., 2300., 2350., 2400., 2450.,
+            2500., 2550., 2600., 2700., 2800., 3000., 3500., 4000.,
+            4500., 5000., 5500., 6000., 7000., 10000.,
+        ],
+        unit="GeV",
+        x_title=r"$m({t}\overline{t})$",
+        y_title="Events",
+    )
+    config.add_variable(
         name="ttbar_mass_ext",
         expression="TTbar.mass",
         # binning=[
